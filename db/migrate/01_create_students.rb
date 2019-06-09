5af2030af10c189ef18f79f24bc6a6c7fbc8fd04
Samuel T <ssamt1142@gmail.com>
2019-06-09 00:02:53 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -2,9 +2,6 @@ class CreateStudents < ActiveRecord::Migration[5.1]
   def change
     create_table :students do |t|
       t.string :name
-      include Enumerable
-
-      def each(&block)
-
-      end
+    end
+  end
 end
